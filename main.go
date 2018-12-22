0a77c7a34c0fe712456dfbbf6204d77fbb352b1c
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-22 19:57:33 +0900

    wasm関数登録

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello, wasm!")
+	c := make(chan struct{}, 0)
+
+	wasmWrapper("getHello", getHello)
+	wasmWrapper("get100", get100)
+	wasmWrapper("echo", echo)
+
+	js.Global().Get("GoWasm").Call("onLoad")
+	<-c
+}
+
+func wasmWrapper(name string, fn func(value js.Value) (interface{}, error)) {
+	handler := js.NewCallback(func(i []js.Value) {
+		fmt.Println("exec " + name)
+
+		if len(i) == 0 {
+			var val js.Value
+			fn(val)
+			return
+		}
+
+		data := i[0].Get("data")
+		res, err := fn(data)
+
+		if err != nil {
+			fail := i[0].Get("fail")
+			if fail != js.Undefined() {
+				fail.Invoke(err.Error())
+			}
+			return
+		}
+
+		done := i[0].Get("done")
+		if done != js.Undefined() {
+			done.Invoke(res)
+		}
+	})
+
+	js.Global().Get("GoWasm").Get("functions").Set(name, handler)
+	js.Global().Get("GoWasm").Call("onAddFunction", name)
+}
+
+func getHello(value js.Value) (interface{}, error) {
+	return fmt.Sprintf("Hello " + value.String()), nil
+}
+
+func get100(value js.Value) (interface{}, error) {
+	return 100, nil
+}
+
+func echo(value js.Value) (interface{}, error) {
+	return value, errors.New("error")
 }
---
+++ public/main.js
@@ -0,0 +1,47 @@
+const GoWasm = {
+  functions: {},
+  onAddFunction(name) {
+    console.log('add wasm function: ', name)
+    const createButton = (name) => {
+      const btn = document.createElement('button')
+      btn.type = 'button'
+      btn.onclick = () => {
+        this.functions[name]({
+          data: { a: 1, b: 2 },
+          done: (arg) => {
+            console.log('done: ', arg)
+          },
+          fail: (err) => {
+            console.error(err)
+          }
+        })
+      }
+      btn.textContent = name
+      document.body.appendChild(btn)
+    }
+    createButton(name)
+  },
+  onLoad () {
+    document.getElementById("wasmReady").textContent = 'wasm ready'
+    console.log('wasm functions loaded')
+  }
+}
+window.GoWasm = GoWasm
+
+if (!WebAssembly.instantiateStreaming) { // polyfill
+  WebAssembly.instantiateStreaming = async (resp, importObject) => {
+  const source = await (await resp).arrayBuffer
+  return await WebAssembly.instantiate(source, importObject)
+  }
+}
+
+const go = new Go()
+let mod, inst
+WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+  mod = result.module
+  inst = result.instance
+  go.run(inst)
+}).catch((err) => {
+  console.error(err)
+})
+
